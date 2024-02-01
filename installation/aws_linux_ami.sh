0f9e8af92ddc6f4c8d03913599c885fa3a96a6e7
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-01 08:25:34 +0100

    Create aws_linux_ami.sh

---
+++ installation/aws_linux_ami.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Upgrade to Java-8
+sudo yum install java-1.8.0
+sudo yum remove java-1.7.0-openjdk
+
+# Installing Artifactory
+wget https://bintray.com/jfrog/artifactory-rpms/rpm -O bintray-jfrog-artifactory-rpms.repo
+sudo mv bintray-jfrog-artifactory-rpms.repo /etc/yum.repos.d/
+sudo yum install jfrog-artifactory-oss
+
+# Setup Service
+sudo chkconfig artifactory on
+sudo service artifactory start
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
