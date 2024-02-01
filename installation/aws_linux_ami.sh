d636211a0c6ba6bb1ef419e46a97693d06e53bd7
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-01 09:42:49 +0100

    Update aws_linux_ami.sh

--- installation/aws_linux_ami.sh
+++ installation/aws_linux_ami.sh
@@ -3,6 +3,11 @@
 sudo yum install java-1.8.0
 sudo yum remove java-1.7.0-openjdk
 
+## Setting up JAVA_HOMO 
+JAVA_HOME=/usr/lib/jvm/
+
+
+
 # Installing Artifactory
 wget https://bintray.com/jfrog/artifactory-rpms/rpm -O bintray-jfrog-artifactory-rpms.repo
 sudo mv bintray-jfrog-artifactory-rpms.repo /etc/yum.repos.d/
