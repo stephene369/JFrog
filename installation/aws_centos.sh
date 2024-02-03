50b49dd709a2ddc1d7938c56f741d69f3f0b299e
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-03 11:22:30 +0100

    Update and rename centos_aws.sh to aws_centos.sh

--- installation/aws_centos.sh
+++ installation/aws_centos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## install Java 
-yum install java-1.8.0-openjdk-devel 
+sudo yum install java-1.8.0-openjdk-devel 
 
 ## Settng docket reposotory up 
 sudo yum install -y yum-utils
@@ -18,15 +18,26 @@ sudo docker run hello-world
 sudo systemctl status docker
 
 ## Pulling Image and lunch Jfrog
-docker pull docker.bintray.io/jfrog/artifactory-oss
+sudo docker pull docker.bintray.io/jfrog/artifactory-oss
 
 ## check if it's download 
-docker images 
+sudo docker images 
 
 ## Run JFrog Artifactory name as a Docker IMage 
-docker rn -it -d -p 8081:8081 --name artifactory docker.bintray.io/jfrog/artifactory-oss
+sudo docker run -it -d -p 8081:8081 --name artifactory1 docker.bintray.io/jfrog/artifactory-oss
+sudo docker run -it -d -p 8082:8082 --name artifactory2 docker.bintray.io/jfrog/artifactory-oss
 
-## check if it running : 
-docker ps
+## check if it running and Logs
+sudo docker ps
+sudo docker logs artifactory1
+sudo docker logs artifactory2
+
+## To remove conteneurs
+sudo docker stop artifactory1
+sudo docker rm artifactory1
+
+sudo docker stop artifactory2
+sudo docker rm artifactory2
+
 
 
