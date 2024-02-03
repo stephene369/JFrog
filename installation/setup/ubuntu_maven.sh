7886aab0c37fcd81f1afbb30f69f8108410d9f0f
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-03 21:57:09 +0100

    Update ubuntu_maven.sh

--- installation/setup/ubuntu_maven.sh
+++ installation/setup/ubuntu_maven.sh
@@ -10,3 +10,15 @@ sudo apt install maven -y
 mvn --version
 
 sudo apt install openjdk-8-jdk -y
+
+echo 'export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export M2_HOME=/opt/apache-maven-3.9.6
+export MAVEN_HOME=/opt/apache-maven-3.9.6
+export PATH=${M2_HOME}/bin:${PATH}' | sudo tee /etc/profile.d/maven.sh
+
+## Make the file executable : 
+sudo chmod +x /etc/profile.d/maven.sh
+
+## Aply environment variables 
+source /etc/profile.d/maven.sh
+
